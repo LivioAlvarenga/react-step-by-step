0563b86bec31bd9191a5197bf61a6b2f9a4f5f13
Livio Alvarenga <livioalvarenga@gmail.com>
2022-11-25 14:12:15 -0300

    add font Inter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,15 +4,19 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        fira: ["Fira Code", "sans-serif"],
-      },
+        inter: ["Inter", "sans-serif"],
+    },
       colors: {
-        font: "#212121",
+        font: "#282c34",
+        fontS: "#6d6d6d",
         primaria: "#23A3BC",
+        secundaria: "#20232a",
         error: "#d50000",
-        normal: "#fafafa",
-        dark: "#f5f5f5",
+        normal: "#ffffff",
+        dark: "#f7f7f7",
         darkest: "#e0e0e0",
+        comment: "#ffe564",
+        destaque: "#bbeffd4d",
       },
     },
   },
